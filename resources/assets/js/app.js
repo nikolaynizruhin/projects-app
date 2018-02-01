ae765eea2e528c6041ef6f3dd3b154d0ddacdf5b
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2018-02-01 11:42:25 +0200

    Add vue router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
-
+import VueRouter from 'vue-router';
+import router from './routes';
 import MyComponent from './components/MyComponent.vue';
+
+Vue.use(VueRouter);
 
 Vue.component('my-component', MyComponent);
 
 const app = new Vue({
+  router,
   el: '#app'
 });
